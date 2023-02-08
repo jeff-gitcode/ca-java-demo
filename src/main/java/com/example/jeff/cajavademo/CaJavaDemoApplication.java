c95ed6ccfa7d562e620672fcee32b967fa3e92f8
jeff-gitcode <jeff.gitcode@gmail.com>
2023-02-08 11:25:50 +1100

    update

--- src/main/java/com/example/jeff/cajavademo/CaJavaDemoApplication.java
+++ src/main/java/com/example/jeff/cajavademo/CaJavaDemoApplication.java
@@ -5,7 +5,6 @@ import org.slf4j.LoggerFactory;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import com.example.jeff.cajavademo.Infrastructure.Persistence.BaseUserRepository;
 
 @SpringBootApplication
 // @EnableJpaRepositories("com.example.jeff.cajavademo.Infrastructure.Persistence")
